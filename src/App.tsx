0c94a644bb97fbe9f52e48fc18b129379aa32085
Diogo Salvador <contact.diogosalvador@gmail.com>
2025-07-22 22:29:31 -0300

    typing component heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <div>
-        <Heading attr={1234}>Olá Mundo</Heading>
-      <h1>Olá mundo!</h1>
+      <Heading>
+        Olá Mundo
+        <button>
+          <TimerIcon/>
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
         incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
