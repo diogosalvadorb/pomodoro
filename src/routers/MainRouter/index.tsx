3222d2e66367c1ab32323c7d286d47da84a72389
diogosalvadorb <contact.diogosalvador@gmail.com>
2025-08-08 17:52:48 -0300

    blib date, history task

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import { Home } from "../../pages/Home";
 import { AboutPomodoro } from "../../pages/AboutPomodoro";
 import { NotFound } from "../../pages/NotFound";
+import { History } from "../../pages/History";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,6 +20,7 @@ export function MainRouter() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/history/' element={<History />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
