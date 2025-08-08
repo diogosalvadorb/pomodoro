7e28f3d9fd18e6f3e29e1fbda688dc30ff1c8ca9
diogosalvadorb <contact.diogosalvador@gmail.com>
2025-08-08 13:06:42 -0300

    routerlink and updating the page title every second

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
 import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router';
+import { RouterLink } from '../RouterLink';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -35,32 +35,32 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <Link
+      <RouterLink
         className={styles.menuLink}
-        to='/'
+        href='/'
         aria-label='Ir para a Home'
         title='Ir para a Home'
       >
         <HouseIcon />
-      </Link>
+      </RouterLink>
 
-      <a
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/history/'
         aria-label='Ver Histórico'
         title='Ver Histórico'
       >
         <HistoryIcon />
-      </a>
+      </RouterLink>
 
-      <a
+      <RouterLink
         className={styles.menuLink}
-        href='#'
+        href='/settings/'
         aria-label='Configurações'
         title='Configurações'
       >
         <SettingsIcon />
-      </a>
+      </RouterLink>
 
       <a
         className={styles.menuLink}
--- src/pages/AboutPomodoro/index.tsx
+++ src/pages/AboutPomodoro/index.tsx
@@ -2,6 +2,7 @@
 import { Container } from '../../components/Container/Index';
 import { GenericHtml } from '../../components/GenericHtml';
 import { Heading } from '../../components/Heading/Index';
+import { RouterLink } from '../../components/RouterLink';
 
 import { MainTemplate } from '../../templates/MainTemplate';
 
@@ -52,8 +53,8 @@ export function AboutPomodoro() {
           <p>
             Você pode configurar o tempo de foco, descanso curto e descanso
             longo do jeito que quiser! Basta acessar a{' '}
-            <a href='/settings'>página de configurações</a> e ajustar os minutos
-            como preferir.
+            <RouterLink href='/settings/'>página de configurações</RouterLink> e
+            ajustar os minutos como preferir.
           </p>
 
           <h3>🔁 Ciclos organizados em sequência</h3>
@@ -98,9 +99,9 @@ export function AboutPomodoro() {
           <h3>📊 Histórico automático</h3>
           <p>
             Todas as suas tarefas e ciclos concluídos ficam salvos no{' '}
-            <a href='/history'>histórico</a>, com status de completas ou
-            interrompidas. Assim, você consegue acompanhar sua evolução ao longo
-            do tempo.
+            <RouterLink href='/history/'>histórico</RouterLink>, com status de
+            completas ou interrompidas. Assim, você consegue acompanhar sua
+            evolução ao longo do tempo.
           </p>
 
           <h2>Por que usar o Chronos Pomodoro?</h2>
@@ -113,8 +114,8 @@ export function AboutPomodoro() {
 
           <p>
             <strong>Pronto pra focar?</strong> Bora lá{' '}
-            <a href='/'>voltar para a página inicial</a> e iniciar seus
-            Pomodoros! 🍅🚀
+            <RouterLink href='/'>voltar para a página inicial</RouterLink> e
+            iniciar seus Pomodoros! 🍅🚀
           </p>
 
           <p>
