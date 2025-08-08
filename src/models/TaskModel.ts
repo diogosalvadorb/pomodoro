6c1f8784535c94a6173fe69fde650639a2d2e46a
diogosalvadorb <contact.diogosalvador@gmail.com>
2025-08-08 18:52:34 -0300

    task status rule, history table order

--- src/models/TaskModel.ts
+++ src/models/TaskModel.ts
@@ -5,7 +5,7 @@ export type TaskModel = {
   name: string;
   duration: number;
   startDate: number;
-  completeDate: number | null; 
-  interruptDate: number | null; 
+  completeDate: number | null; // quando o timer chega ao final
+  interruptDate: number | null; // quando a task for interrompida
   type: keyof TaskStateModel['config'];
 };
