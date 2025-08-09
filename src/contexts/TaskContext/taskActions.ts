b6d7fb04831fd5e57e6f94d0c1198323ef9d00ab
diogosalvadorb <contact.diogosalvador@gmail.com>
2025-08-09 20:37:27 -0300

    saving the state settings

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -1,4 +1,12 @@
+// useReducer <- hook do React que recebe um reducer e um estado inicial
+// reducer <- função que recebe o estado atual e uma ação, e retorna o novo estado
+// state <- o estado atual
+// action <- a ação disparada, geralmente é um objeto com type e (opcionalmente) payload
+// type <- o tipo da ação, geralmente uma string (pode ser enum, constante, etc)
+// payload <- os dados extras enviados junto com a action, se necessário para atualizar o estado
+
 import type { TaskModel } from '../../models/TaskModel';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
 export const TaskActionTypes = {
   START_TASK: 'START_TASK',
@@ -6,6 +14,7 @@ export const TaskActionTypes = {
   RESET_STATE: 'RESET_STATE',
   COUNT_DOWN: 'COUNT_DOWN',
   COMPLETE_TASK: 'COMPLETE_TASK',
+  CHANGE_SETTINGS: 'CHANGE_SETTINGS',
 } as const;
 
 export type TaskActionTypes =
@@ -19,6 +28,10 @@ export type TaskActionsWithPayload =
   | {
       type: typeof TaskActionTypes.COUNT_DOWN;
       payload: { secondsRemaining: number };
+    }
+  | {
+            type: typeof TaskActionTypes.CHANGE_SETTINGS;
+      payload: TaskStateModel['config'];
     };
 
 export type TaskActionsWithoutPayload =
